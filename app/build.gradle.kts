54bfd07e033bd35cd60f3f675fd4ee257281211c
Ehtisham Ali <aliehtisham052-dotcom@users.noreply.github.com>
2026-07-12 00:49:24 +0000

    v6.41: About tab heading now 'Powered by' instead of 'Developer'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.innovation313.glowedge"
         minSdk = 26
         targetSdk = 36
-        versionCode = 76
-        versionName = "6.40"
+        versionCode = 77
+        versionName = "6.41"
     }
 
     signingConfigs {
